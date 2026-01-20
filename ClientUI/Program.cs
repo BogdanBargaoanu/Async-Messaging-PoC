b5da342ad5de16ee5de375a920b5542d76aa431b
Bogdan Bargaoanu Alexandru <bn19bba@yahoo.com>
2026-01-20 09:58:29 +0200

    feat: implemented client UI

---
+++ ClientUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+
+Console.Title = "ClientUI";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("ClientUI");
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+// Create .learningtransport directory in project root
+var projectRoot = Directory.GetParent(Directory.GetCurrentDirectory())?.FullName
+                  ?? Directory.GetCurrentDirectory();
+var storageDirectory = Path.Combine(projectRoot, ".learningtransport");
+if (!Directory.Exists(storageDirectory))
+{
+    Directory.CreateDirectory(storageDirectory);
+}
+;
+
+var transport = endpointConfiguration.UseTransport(new LearningTransport());
+
+builder.UseNServiceBus(endpointConfiguration);
+
+var app = builder.Build();
+
+await app.RunAsync();
